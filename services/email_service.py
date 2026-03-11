6559c1f597562f383cf50d60a182d75472ea6590
Gustavo Antonio <guty.2344@gmail.com>
2026-03-11 20:26:02 -0300

    fix smtp config

--- services/email_service.py
+++ services/email_service.py
@@ -5,10 +5,11 @@ conf = ConnectionConfig(
     MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
     MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
     MAIL_FROM=os.getenv("MAIL_FROM"),
-    MAIL_PORT=int(os.getenv("MAIL_PORT")),
-    MAIL_SERVER=os.getenv("MAIL_SERVER"),
+    MAIL_PORT=587,
+    MAIL_SERVER="smtp.gmail.com",
     MAIL_STARTTLS=True,
     MAIL_SSL_TLS=False,
+    USE_CREDENTIALS=True
 )
 
 async def send_user_credentials(email: str, username: str, password: str):
